d844f921d42636d47d0c9422a8f5102e4d6a84c8
Shinuk Yi <wook3024@gmail.com>
2022-04-04 15:15:10 +0900

    Add weather schemas

---
+++ app/schemas/weather.py
@@ -0,0 +1,49 @@
+from pydantic import BaseModel
+
+
+class Summary(BaseModel):
+    greeting: str
+    temperature: str
+    heads_up: str
+
+
+class SummaryResponse(BaseModel):
+    summary: Summary
+
+
+class Coordinate(BaseModel):
+    lat: float
+    lon: float
+
+
+class CurrentWeatherRequest(Coordinate):
+    ...
+
+
+class CurrentWeatherResponse(BaseModel):
+    timestamp: float
+    code: int
+    temp: float
+    rain1h: int
+
+
+class ForecastWeatherRequest(Coordinate):
+    hour_offset: int
+
+
+class ForecastWeatherResponse(BaseModel):
+    timestamp: float
+    code: int
+    min_temp: float
+    max_temp: float
+    rain: int
+
+
+class HistoricalWeatherRequest(Coordinate):
+    hour_offset: int
+
+
+class HistoricalWeatherResponse(BaseModel):
+    timestamp: float
+    code: int
+    rain1h: int
