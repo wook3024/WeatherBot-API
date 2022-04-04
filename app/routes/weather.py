9b41bfb967f788d61a7725fa2ffb35e0b96b050c
Shinuk Yi <wook3024@gmail.com>
2022-04-05 08:36:13 +0900

    Add logger in weather process

--- app/routes/weather.py
+++ app/routes/weather.py
@@ -5,7 +5,7 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.responses import ORJSONResponse
 from httpx import AsyncClient
 
-from .. import cfg, schemas
+from .. import cfg, logger, schemas
 from ..utils.weather import Greeting, HeadsUp, Temperature, Weather
 
 router = APIRouter()
@@ -13,6 +13,7 @@ router = APIRouter()
 
 @router.get("/summary", response_model=schemas.SummaryResponse)
 async def summary(lon: float, lat: float) -> ORJSONResponse:
+    logger.info("Request data \nlon: {}, lat: {}".format(lon, lat))
     async with AsyncClient() as client:
         cur_weather, pre_weather = await asyncio.gather(
             Weather.request(
@@ -30,13 +31,15 @@ async def summary(lon: float, lat: float) -> ORJSONResponse:
                 endpoint=cfg.service.weather.historical_endpoint,
             ),
         )
+    logger.debug("Current weather \n{}".format(cur_weather))
+    logger.debug("Previous weather \n{}".format(pre_weather))
     greeting_message, temp_message, headsup_message = await asyncio.gather(
         Greeting.get_greeting_message(schemas.CurrentWeatherResponse(**cur_weather)),
         Temperature.get_temp_message(
             lat=lat,
             lon=lon,
-            cur_temp=cur_weather.get("temp", float("inf")),
-            pre_temp=pre_weather.get("temp", float("inf")),
+            cur_temp=cur_weather.get("temp"),
+            pre_temp=pre_weather.get("temp"),
             hour_offset=-24,
         ),
         HeadsUp.get_headsup_message(lat, lon),
